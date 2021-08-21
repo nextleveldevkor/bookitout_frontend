9819dc613ccde15da5992fe6f2d2726a2e05b556
Jina0202 <81006122+Jina0202@users.noreply.github.com>
2021-08-21 15:01:09 +0900

    first commit (2)

---
+++ js/col-4.js
@@ -0,0 +1,7 @@
+const images=["Book 1.jpg","Book 2.jpg","Book 3.jpg","Book 4.jpg","Book 5.jpg","Book 6.jpg","Book 7.jpg","Book 8.jpg","Book 9.jpg","Book 10.jpg","Book 11.jpg","Book 12.jpg",];
+
+for (var i=0;i<images.length;i++){
+const eachImg=document.createElement("img");
+eachImg.src=`images/${images[i]}`;
+document.body.
+}
